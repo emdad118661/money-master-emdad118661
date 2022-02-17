b76840d7d146aff3a99dd9ce6f4cea3a00cf0dc8
emdad118661 <emdad118661@gmail.com>
2022-02-17 18:30:59 +0600

    calculate event handler

--- js/calculation.js
+++ js/calculation.js
@@ -1,3 +1,4 @@
+// function for get input from user 
 function getInputValue(inputId) {
     const getInput = document.getElementById(inputId);
     const getInputText = getInput.value;
@@ -5,13 +6,31 @@ function getInputValue(inputId) {
     getInput.value = '';
     return getInputValue;
 }
+
 document.getElementById('calculate-button').addEventListener('click', function () {
     const incomeAmount = getInputValue('income-input');
     const foodExpenses = getInputValue('food-input');
     const rentExpenses = getInputValue('rent-input');
     const clothsExpenses = getInputValue('cloths-input');
-    const totalExpenses = foodExpenses + rentExpenses + clothsExpenses;
-    const totalBalance = incomeAmount - totalExpenses;
-    console.log(totalExpenses);
-    console.log(totalBalance);
+
+    //update total expenses
+    const expensesInnerText = document.getElementById('total-expenses');
+    const previousExpenseText = expensesInnerText.innerText;
+    const previousExpenseAmount = parseFloat(previousExpenseText);
+
+    const totalExpenses = foodExpenses + rentExpenses + clothsExpenses + previousExpenseAmount;
+    expensesInnerText.innerText = totalExpenses;
+
+    //update total balance
+    const balanceInnerText = document.getElementById('total-balance');
+    const previousBalanceText = balanceInnerText.innerText;
+    const previousBalanceAmount = parseFloat(previousBalanceText);
+
+    const newBalance = incomeAmount - totalExpenses;
+    const totalBalance = previousBalanceAmount + newBalance;
+    balanceInnerText.innerText = totalBalance;
+
+    // const totalBalance = incomeAmount - totalExpenses;
+    // console.log(totalExpenses);
+    // console.log(totalBalance);
 })
